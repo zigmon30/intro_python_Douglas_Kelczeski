6656d5911afe140ab3d324684049be26bcc509dc
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-21 20:39:06 -0300

    update desafio 104

--- desafios/104.java
+++ desafios/104.java
@@ -1,13 +1,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+// ex1 ##########################################################
 		int x = 50, y = 100, z = x, zz = y;
 		y = z; x = zz;
 
 		System.out.println(x);
 		System.out.println(y);
+//###############################################################
 
+// ex2 ##########################################################
+		int w = 50, h = 70;  
+		w = w + h;
+		h = w - h;
+		w = w - h;
+		System.out.println(w);
+		System.out.println(h);
+//###############################################################		
 	}
 
 }
