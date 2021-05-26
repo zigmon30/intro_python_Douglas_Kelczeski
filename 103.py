318212ff1cc75fd3b259403dfc7c40ef9603ebc2
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-26 20:03:52 -0300

    update 103
    corrigido conforme pedido professor

--- 103.py
+++ 103.py
@@ -7,23 +7,24 @@ book3 = 'Fooled by Randomness by Nassim Nicholas Taleb, 2001'
 
 # 1) Extraia o titulo do livro da string
 titulo_extraido = book1[0:9:1]
-print(f'ex1\n{titulo_extraido}')
+print(f'ex1\n{titulo_extraido}\n')
 
 
 # 2) Salve o titulo de cada livro em uma variável
 titulo1 = book1[0:9]
-titulo2 = book2[0:12]
+titulo2 = book2[0:11]
 titulo3 = book3[0:6]
+print(f'ex2\n{titulo1} / {titulo2} / {titulo3}\n')
 
 # 3) Quantos caracteres cada título tem?
-print(len(titulo1))
-print(len(titulo2))
-print(len(titulo3))
+print(f'ex3\nTamanho titulo1 {len(titulo1)}')
+print(f'Tamanho titulo2 {len(titulo2)}')
+print(f'Tamanho titulo3 {len(titulo3)}\n')
 
 # 4) Imprima com a formatacao: {Titulo} - {Autor}, {Ano}
 
 titulo1 = book1[0:9]
-titulo2 = book2[0:12]
+titulo2 = book2[0:11]
 titulo3 = book3[0:6]
 autor_titulo1 = book1[13:30]
 autor_titulo2 = book2[15:36]
@@ -32,19 +33,9 @@ ano_titulo1 = book1[32:36]
 ano_titulo2 = book2[38:42]
 ano_titulo3 = book3[47:51]
 
-print(book1)
-print(book2)
-print(book3)
-print(titulo1)
-print(titulo2)
-print(titulo3)
-print(autor_titulo1)
-print(autor_titulo2)
-print(autor_titulo3)
-print(ano_titulo1)
-print(ano_titulo2)
-print(ano_titulo3)
-print('Hi, livro: {} é do autor: {} do ano: {}!'.format(titulo1, autor_titulo1, ano_titulo1,))
+print(f'ex4\nTitulo: {titulo1} Autor: {autor_titulo1} Ano: {ano_titulo1}')
+print(f'Titulo: {titulo2} Autor: {autor_titulo2} Ano: {ano_titulo2}')
+print(f'Titulo: {titulo3} Autor: {autor_titulo3} Ano: {ano_titulo3}\n')
 
 
 # 5) Verifique se uma palavra é uma palindrome perfeita.
