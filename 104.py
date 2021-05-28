e67f9fbffc6df92ff26b778af2805b25e6716188
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-28 12:24:56 -0300

    update 104
    atualizado ex2 para receber 2 strings e comparalas entre si se são palindromes

--- 104.py
+++ 104.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 '''
     DESAFIO!!!
     1) Implemente um algoritmo para trocar o conteudo de duas variáveis x e y.
@@ -22,19 +20,18 @@ comparando = compara_listas(lista1, lista2)
 print(comparando)
 
 # 2) Crie uma funcao que verifica se duas strings são palindromes perfeitas. Faca as 'limpeza'/sanitizacao necessarias.  Retorne True ou False.
+
 frase = str(input('digite uma frase: ')).upper()
-def compara_palindromo(frase):
+frase1 = str(input('digite outra frase: ')).upper()
+
+def compara_palindromo(frase, frase1):
     palavra = frase.split()
-    palavras_junto = ''.join(palavra)
-    palavras_junto_inverso = palavras_junto[::-1]
-    resultado_palindrome = True if palavras_junto_inverso == palavras_junto else False
+    palavra1 = frase1.split()
+
+    palavras_junto1 = ''.join(palavra)
+    palavras_junto2 = ''.join(palavra1)
+    resultado_palindrome = True if palavras_junto1 == palavras_junto2 else False
     return resultado_palindrome
 
-palindrome = compara_palindromo(frase)
+palindrome = compara_palindromo(frase, frase1)
 print(palindrome)
-
-'''lista1 = [1, 2, 3]
-lista2 = [1, 2, 3]
-comparando = compara_listas(lista1, lista2)
-'''
-# OBS.: Utilize apenas o que foi estudado ate agora
