bbe947715f2b4106d99c5d51d79d5dc115991c08
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-30 14:42:45 -0300

    create desafio 105

--- desafios/103.java
+++ desafios/103.java
@@ -19,5 +19,4 @@ public class Main {
 
 		sc.close();
 	}
-
 }
---
+++ desafios/105.java
@@ -0,0 +1,6 @@
+/*
+    DESAFIO!!!
+    1) Crie uma lista com os 1000 primeiros numeros pares. Não use loop!
+    2) Crie uma lista com os numeros de 0 ate 99999999999999999999999999. Depois crie um loop para percorrer a lista ateh encontrar o primeiro multiplo de 5.
+    OBS.: Use sua linguagem de programacao favorita. Nao use funcoes/metodos prontos.
+*/
