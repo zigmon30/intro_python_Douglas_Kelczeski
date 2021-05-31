f915b4bebb5c9082da5e8421ecdcca8d8c8aec47
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-30 22:07:58 -0300

    update

--- desafios/d3.java
+++ desafios/d3.java
@@ -14,7 +14,7 @@ public class d3 {
 		System.out.println("Voce digitou: ");
 		System.out.println(s1);
 		
-    System.out.println("palavra invertida: ");
+    	System.out.println("palavra invertida: ");
 		System.out.println();
 
 		sc.close();
