c67614562b0d5562769a338f2852a2d79d32869d
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-05-30 22:32:35 -0300

    update

--- desafios/d3.java
+++ desafios/d3.java
@@ -1,22 +1,13 @@
-import java.util.Scanner;
-
 public class d3 {
 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		String s1;
-
-
-		System.out.println("Digite uma frase");
-		s1 = sc.nextLine();
-		System.out.println("Voce digitou: ");
-		System.out.println(s1);
-		
-    	System.out.println("palavra invertida: ");
-		System.out.println();
-
-		sc.close();
-	}
+        String frase = "DOUGLAS KELCZESKI"; 
+        String inverte="";
+        
+        for(int x=frase.length()-1;x>=0;x--){
+            inverte+= frase.charAt(x);
+        }
+        System.out.println(inverte);
+    }
 }
+	
