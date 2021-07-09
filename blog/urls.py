fa34ed83239ac6ea57d875fe92adbc0c8ac537a3
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-07-09 19:32:06 -0300

    up

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 
 ]
