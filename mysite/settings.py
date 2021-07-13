a3fcc3f425321513dc7f813bf37d8bf455bdd6dc
DOUGLAS KELCZESKI <54138968+zigmon30@users.noreply.github.com>
2021-07-13 13:25:53 -0300

    update to mysql server

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'bloguser',
+        'PASSWORD': 'Spring56@Spring56',
+        'HOST': 'localhost',
+        'PORT': '',
+}
 }
 
 
